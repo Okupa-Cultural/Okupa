7472bdd2ef3bb436ad8f211e852845cefbbcf11a
hidanscript <hidanshira2@gmail.com>
2019-08-28 23:06:06 -0300

    Colocado texto de hecho por David

--- Frontend/okupa/src/App.js
+++ Frontend/okupa/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Okupa
+          (Este cambio lo hizo David)
         </p>
         <a
           className="App-link"
