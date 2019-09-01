6d99e96acde05f2f94a8cfe5db23a935dfab57dd
hidanscript <hidanshira2@gmail.com>
2019-09-01 20:20:46 -0300

    Added PSD files

--- Frontend/okupa/src/App.js
+++ Frontend/okupa/src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router, Route } from 'react-router-dom';
+import HomeScreen from './OkupaFiles/Screens/HomeScreen';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Okupa
-          (Este cambio lo hizo David)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn ReactxD
-        </a>
-      </header>
+      <HomeScreen/>
     </div>
   );
 }
