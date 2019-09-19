ce7afa98b22cb3cf66717f8ec0b7edbbce7e83c5
hidanscript <hidanshira2@gmail.com>
2019-09-19 16:05:11 -0300

    Se agrego fondo a la Dashboard, boton Inicio en Dashboard

--- Frontend/okupa/src/OkupaFiles/Screens/Dashboard.js
+++ Frontend/okupa/src/OkupaFiles/Screens/Dashboard.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Router, Route, Link } from 'react-router-dom';
 import Logo from './../../Images/Logo/okupa-logo-colorYtitulo-transparente.png';
 import './css/HomeScreen.css';
+import Fondo from './../../Images/bg2.jpg';
 
-function Dashboard() {
-  return (
-    <div className="home-screen">
-        <div className="banner">
-            <img src={Logo} />
-            <h1>Dashboard</h1>
-            <p>Placeholder Text</p>
-            <button className="boton-registro">Registarse</button>
+export default class Dashboard extends Component {
+
+    render() {
+
+         var sectionStyle = {
+          zIndex: -10,
+          position: 'absolute',
+          width: "100%",
+          height: "100%",
+          backgroundImage: "url(" + Fondo + ")"
+        };
+
+      return (
+        <div className="home-screen">
+
+         <div style={ sectionStyle } />
+
+            <div className="banner">
+                <img src={Logo} />
+                <h1>Dashboard</h1>
+                <p>Placeholder Text</p>
+                <Link to="/">
+                    <input type="submit" className="boton-registro" value="Inicio"/>
+                </Link>
+            </div>
         </div>
-    </div>
-  );
+      );
+    }    
 }
-
-export default Dashboard;
