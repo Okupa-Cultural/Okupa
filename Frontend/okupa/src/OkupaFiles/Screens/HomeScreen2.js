5a4b9fb6e41303442aae86698e6200195b090ca1
Chicho <lgarcialuzzi@gmail.com>
2019-10-29 23:36:16 -0300

    Hice unas pocas modificaciones a la version movil de HomeScreen2

--- Frontend/okupa/src/OkupaFiles/Screens/HomeScreen2.js
+++ Frontend/okupa/src/OkupaFiles/Screens/HomeScreen2.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import DatePicker from "react-datepicker";
 
 //Imagenes
@@ -46,12 +47,24 @@ export default class HomeScreen2 extends Component {
 				<input type="checkbox" id="burguer" />
 
 	    		<nav className="home2-botonera">
-	    			<div className="nav-boton">HOME</div>
-	    			<div className="nav-boton">CARACTERÍSTICAS</div>
-	    			<div className="nav-boton">QUIÉNES SOMOS</div>
-	    			<div className="nav-boton">CONTACTO</div>
-	    			<div className="nav-boton">REGÍSTRATE</div>
-	    			<div className="nav-boton resaltado">LOGIN</div>
+	    			<Link to="/homedos">
+	    				<div className="nav-boton">HOME</div>
+	    			</Link>
+	    			<Link to="/homedos">
+	    				<div className="nav-boton">CARACTERÍSTICAS</div>
+	    			</Link>
+	    			<Link to="/homedos">
+	    				<div className="nav-boton">QUIÉNES SOMOS</div>
+	    			</Link>
+	    			<Link to="/homedos">
+	    				<div className="nav-boton">CONTACTO</div>
+	    			</Link>
+	    			<Link to="/homedos">
+	    				<div className="nav-boton">REGÍSTRATE</div>
+	    			</Link>
+	    			<Link to="/homedos">
+	    				<div className="nav-boton resaltado">LOGIN</div>
+	    			</Link>
 	    		</nav>
 
     		</div>
@@ -59,22 +72,21 @@ export default class HomeScreen2 extends Component {
     		<div className="home2-presentacion">
 
 				<p className="home2-intro">
-					Una nueva forma de <span>difundir&nbsp;</span> y <span>conectar&nbsp;</span>
-					con el arte indpendiente
+					Una nueva forma de <span>difundir&nbsp;</span>y <span>conectar&nbsp;</span>
+					con el arte indpendeiente
 				</p>
 
 				<form className="home2-form">
 					<label className="home2-label">
-						<h4>Buscar eventos</h4>
+						<h4>Buscar eventos por su nombre</h4>
 						<input type="text" className="home2-searchbar" name="busqueda" autocomplete="off" placeholder="Busca un evento por su nombre..."/>
 					</label>
 					<label className="home2-label">
-						<h4 className="mrhide">Buscar eventos</h4>
+						<h4>o por su fecha</h4>
 						<DatePicker 
 						className="home2-datepicker"
 						selected={this.state.startDate}
 						onChange={this.handleChange}
-						value="ó por su fecha :)"
 						/>
 					</label>
 				</form>
