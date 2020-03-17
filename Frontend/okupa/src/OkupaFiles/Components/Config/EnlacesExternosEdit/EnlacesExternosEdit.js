55833b4c763aee48dc349ae288c8b98cface29a1
Chicho <lgarcialuzzi@gmail.com>
2020-03-17 14:20:13 -0300

    algunos cambios que he estado haciendo, nada del otro mundo

---
+++ Frontend/okupa/src/OkupaFiles/Components/Config/EnlacesExternosEdit/EnlacesExternosEdit.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+//CSS
+import '../css/EnlacesExternosEdit.css'
+
+export default class EnlacesExternosEdit extends Component {
+
+    render() {
+        return(
+            <section className="enlaces-externos config-seccion">
+                <h2 className="seccion-titulo">Enlaces externos</h2>
+                <form className="enlaces-contenedor">
+                    <button formAction="https://instagram.com" formTarget="_blank">
+                    <i className="fab fa-instagram"></i>
+                    <span className="leyenda">Instagram</span>
+                    </button>
+                    <button formAction="https://soundcloud.com" formTarget="_blank">
+                    <i className="fab fa-soundcloud"></i>
+                    <span className="leyenda">Soundcloud</span>
+                    </button>
+                    <button formAction="https://Bandcamp.com" formTarget="_blank">
+                    <i className="fab fa-bandcamp"></i>
+                    <span className="leyenda">Bandcamp</span>
+                    </button>
+                    <button formAction="https://facebook.com" formTarget="_blank">
+                    <i className="fab fa-facebook-square"></i>
+                    <span className="leyenda">Facebook</span>
+                    </button>
+                    <button formAction="http://www.alternativateatral.com" formTarget="_blank">
+                    <i className="fas fa-link"></i>
+                    <span className="leyenda">Alternativa teatral</span>
+                    </button>
+                    <button formAction="https://twitter.com" formTarget="_blank">
+                    <i className="fab fa-twitter"></i>
+                    <span className="leyenda">Twitter</span>
+                    </button>
+                    
+                </form>
+            </section>
+        );
+    }
+}
---
+++ Frontend/okupa/src/OkupaFiles/Screens/Config.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+
+//Componentes
+import OkInput from '../Components/Generales/OkInput/OkInput';
+import OkTextarea from '../Components/Generales/OkTextarea/OkTextarea';
+import SelectorEtiquetas from '../Components/Generales/SelectorEtiquetas/SelectorEtiquetas';
+import GaleriaEdit from '../Components/Config/GaleriaEdit/GaleriaEdit';
+import EnlacesExternosEdit from '../Components/Config/EnlacesExternosEdit/EnlacesExternosEdit';
+import OkTabla from '../Components/Generales/OkTabla/OkTabla';
+import OkRegistro from '../Components/Generales/OkTabla/OkRegistro';
+
+//CSS
+import './css/EstilosGenerales.css'
+import './css/Config.css';
+
+export default class Config extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: 'pownie_rocks',
+            email: 'leslieknope_unicorns@gmail.com',
+            password: 'contrasenaencriptada',
+            proyectname: 'Leslie Knope',
+            formationdate: '21/10/2010',
+            bio: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, asperiores. Recusandae vel provident iste deserunt assumenda laboriosam ipsam sint explicabo, dolores aut, quam itaque hic. Aperiam, ipsa! Totam, sunt voluptas!',
+        };
+    }
+
+    componentDidMount (){
+        document.getElementById('username').defaultValue = this.state.username;
+        document.getElementById('email').defaultValue = this.state.email;
+        document.getElementById('password').defaultValue = this.state.password;
+        document.getElementById('proyectname').defaultValue = this.state.proyectname;
+        document.getElementById('formationdate').defaultValue = this.state.formationdate;
+        document.getElementById('bio').defaultValue = this.state.bio;
+
+    }
+
+    render(){
+        return(
+            <div className="config">
+                <section>
+                    <h1 className="titulo">
+                        <i className="far fa-chevron-left"></i>
+                        Configuración
+                    </h1>
+				</section>
+                <section className="config-seccion">
+                    <h2 className="seccion-titulo">General</h2>
+                    <div className="seccion-cuerpo">
+                        <div className="config-input">
+                            <label htmlFor="username">
+                                Nombre de usuario
+                            </label>
+                            <OkInput Type="text" Name="username" />
+                        </div>
+                        <div className="config-input">
+                            <label htmlFor="email">
+                                Email
+                            </label>
+                            <OkInput Type="email" Name="email" />
+                        </div>
+                        <div className="config-input">
+                            <label htmlFor="password">
+                                Password
+                            </label>
+                            <OkInput Type="password" Name="password" />
+                        </div>
+                    </div>
+                </section>
+                <section className="config-seccion">
+                    <h2 className="seccion-titulo">Proyecto</h2>
+                    <div className="seccion-cuerpo">
+                        <div className="config-input">
+                            <label htmlFor="proyectname">
+                                Nombre del proyecto
+                            </label>
+                            <OkInput Type="text" Name="proyectname" />
+                        </div>
+                        <div className="config-input">
+                            <label htmlFor="formationdate">
+                                Fecha de formación
+                            </label>
+                            <OkInput Type="date" Name="formationdate" />
+                        </div>
+                        <div className="config-input">
+                            <label htmlFor="bio">
+                                Biografía
+                            </label>
+                            <OkTextarea Name="bio" Rows="8" />
+                        </div>
+                    </div>
+                </section>
+                <section className="config-seccion">
+                    <h2 className="seccion-titulo">Etiquetas</h2>
+                    <SelectorEtiquetas />
+                </section>
+
+                <GaleriaEdit />
+
+                <EnlacesExternosEdit />
+
+                <section className="rider config-seccion">
+                    <h2 className="seccion-titulo">Rider técnico</h2>
+                    <OkTabla  Titulo="Elementos de escenario">
+                        <OkRegistro Cantidad="100" Descripcion="Me siento feliz" />
+                    </OkTabla>
+                    <OkTabla  Titulo="Elementos de iluminacion">
+                        <OkRegistro Cantidad="100" Descripcion="Me siento feliz" />
+                    </OkTabla>
+                </section>
+                
+            </div>
+        );
+    }
+}
